02314b279dc78ab4941fd8f3b870c9712ae4dfb1
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-03 19:42:29 +0000

    Local1 and Local2 added

---
+++ src/Local1.java
@@ -0,0 +1,6 @@
+public class Local1 {
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+        System.out.println("yeni satir eklendi");
+    }
+}
---
+++ src/Local2.java
@@ -0,0 +1,6 @@
+public class Local2 {
+    public static void main(String[] args) {
+        System.out.println("test1");
+        System.out.println("test2");
+    }
+}
