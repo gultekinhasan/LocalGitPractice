c6d7b3445661b6bd7a86cff21266bb7b3ef83185
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-05 19:45:01 +0000

    Local2  updated

--- src/Local2.java
+++ src/Local2.java
@@ -2,6 +2,7 @@ public class Local2 {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
-        System.out.println("2-test2");
+        System.out.println("test3");
+        System.out.println("2-test3");
     }
 }
