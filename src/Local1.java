006eb37fbb4cce04f57c21e21386d2072334feb7
Hasan GULTEKIN <av.gultekinhasan@gmail.com>
2024-01-05 18:17:49 +0000

    Local1 and Local2 uploaded

--- src/Local1.java
+++ src/Local1.java
@@ -2,5 +2,6 @@ public class Local1 {
     public static void main(String[] args) {
         System.out.println("Git Test");
         System.out.println("yeni satir eklendi");
+        System.out.println(" 2.yeni satir eklendi");
     }
 }
--- src/Local2.java
+++ src/Local2.java
@@ -2,5 +2,6 @@ public class Local2 {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("2-test2");
     }
 }
