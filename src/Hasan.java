462170e400ff86d29f56e497d089f09adb232755
Hasan Gultekin <146466391+gultekinhasan@users.noreply.github.com>
2024-01-05 18:46:51 +0000

    Update from github Hasan.java

--- src/Hasan.java
+++ src/Hasan.java
@@ -1,5 +1,7 @@
 public class Hasan {
     public static void main(String[] args) {
-        System.out.println("Hasan branch'ın da eklenen code!!!");
+        System.out.println("Hasan branch'ın da eklenen code!!!, buraya ekleme yapıldı....");
+        // yeni satir
+        System.out.println("githubdan buraya ekleme yapıldı....");
     }
 }
